6d0c3e5e6dd7455f35c52e4878eb830517e85d84
LloydMatare <lloydsmatare@gmail.com>
2026-03-03 16:09:25 +0200

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260303_094515 from './20260303_094515';
+
 
 export const migrations = [
-  {
-    up: migration_20260303_094515.up,
-    down: migration_20260303_094515.down,
-    name: '20260303_094515'
-  },
+  // {
+  //   up: migration_20260303_094515.up,
+  //   down: migration_20260303_094515.down,
+  //   name: '20260303_094515'
+  // },
 ];
